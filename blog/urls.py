02e88c82b4602e514f48c2c95a945f07ed4dd0c7
tianshizhang <steventsz@outlook.com>
2026-03-30 18:24:44 +1300

    post create generic view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from blog.views import index
-
+from blog.views import index, category_detail, post_detail, create_category, PostListView, PostDetailView, \
+    PostCreateView
 
 urlpatterns = [
-    path('', index, name = 'index')
+    path('', index, name = 'index'),
+    path('category/<int:category_id>/', category_detail, name = 'category_detail'),
+    path('post/<int:post_id>/', post_detail, name = 'post_detail'),
+    path('create_category/', create_category, name = 'create_category'),
+    path('post/', PostListView.as_view(), name = 'post_list'),
+    path('post_detail/<int:pk>/detail/', PostDetailView.as_view(), name='post_detail_view'),
+
+    path('post_create/', PostCreateView.as_view(), name='post_create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from unicodedata import category
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
-from django.shortcuts import render
-
-from blog.models import Category
+from blog.models import Category, Post
 
 
 # Create your views here.
@@ -10,3 +11,48 @@ def index(request):
     categories = Category.objects.all()
     return render(request, 'blog/index.html',
                   {'categories': categories})
+
+def category_detail(request, category_id):
+    category = Category.objects.get(id=category_id)
+    return render(request, 'blog/category.html',
+                  {'category': category})
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'blog/post.html',
+                  {'post': post})
+
+def create_category(request):
+    if request.method == 'POST':
+        name = request.POST['category_name']
+        category = Category.objects.create(name=name)
+        return redirect('category_detail', category_id=category.id)
+    return render(request, 'blog/create_category.html')
+
+# def create_post(request):
+#     if request.method == 'POST':
+#         title = request.POST['post_title']
+#         body = request.POST['post_content']
+#         category_id = request.POST['category_id']
+#         author_id = request.POST['author_id']
+#         author = User.objects.get(id=author_id)
+#
+#         category = Category.objects.get(id=category_id)
+#         post = Post.objects.create(title=title, category=category, body=body, Auther=author)
+#         return redirect('post_detail', post_id=post.id)
+#     return render(request, 'blog/create_post.html')
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'  # Default: object_list
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'posts'  # Default: object_list
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_create.html'
+    fields = ['title', 'body', 'category', 'snippet', 'header_image', 'auther']
+    success_url = reverse_lazy('post_list')
